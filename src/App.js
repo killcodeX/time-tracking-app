37277eb61f98da18a7af8e3b935e158c63fa148a
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-12-17 20:50:26 +0530

    fontawesome added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+import React from "react";
+import Header from './components/layout/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <button className='btn btn-primary'>HI</button>
     </div>
   );
---
+++ src/components/layout/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <nav class="navbar navbar-expand-lg nav-bg">
+      <p class="navbar-brand text-white" href="#">
+      <i class="fas fa-stopwatch-20"></i> Time Tracking App
+      </p>
+    </nav>
+  );
+}
